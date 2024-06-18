8f1ee4f19bb286b1ae0e3a31930e7e2232d261a9
MickaelSuard <87171726+MickaelSuard@users.noreply.github.com>
2024-06-18 19:27:05 +0200

    Update readme.js

--- readme.js
+++ readme.js
@@ -1,13 +1,19 @@
-module.exports = `### Hi there 👋
+module.exports = `Hi there 👋
+
 
 ### Visitor count
 
+
 <img src="https://profile-counter.glitch.me/MickaelSuard/count.svg" />
+
 
 Added the \`Mon 03 Jun 2024\`
 
+
 Last update on <#today_date>
 
+
 <#day_before_new_years> days before new years
+
 
 `;
