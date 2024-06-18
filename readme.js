a73945d9d93e61e4ef8fca4cb582cc0ea22cbd29
MickaelSuard <87171726+MickaelSuard@users.noreply.github.com>
2024-06-18 20:41:06 +0200

    Update readme.js

--- readme.js
+++ readme.js
@@ -4,17 +4,6 @@ Hi there 👋,
 
 Welcome to my GitHub profile! Today is <#day_of_the_week>, so let's make it awesome!
 
-### Visitor count
-
-<img src="https://profile-counter.glitch.me/MickaelSuard/count.svg" />
-
-Added the \`Mon 03 Jun 2024\` my project
-
-Last update on <#today_date>
-
-<#day_before_new_years> days before new years
-
-
 ### About Me
 I am Mickael Suard, a developer passionate about coding and new technologies. 
 I love exploring, learning, and sharing my discoveries. 
@@ -31,6 +20,12 @@ When I'm not coding, you might find me sipping a strong coffee or playing video 
 
 ### Contact Me
 Feel free to reach out to me via LinkedIn. I'm always up for a chat about tech, projects, or just about anything!
+
+### Visitor count
+
+<img src="https://profile-counter.glitch.me/MickaelSuard/count.svg" />
+
+Added the \`Mon 03 Jun 2024\` my project
 
 Latest Updates
 Added the project on Mon 03 Jun 2024.
